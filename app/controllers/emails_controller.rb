059e2dac57e9a1aa205d58acf5684dc17ac2efc9
alexis <dupray_a@hotmail.fr>
2023-06-01 19:17:33 +0200

    Gmail-like-Hotwire

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,43 @@
+class EmailsController < ApplicationController
+  def index
+    @emails = Email.all
+    @email = Email.new
+  end
+
+  def show
+    @email = Email.find(params[:id])
+    @email.update(read: true)
+  end
+
+  def new
+    @email = Email.new
+  end
+
+  def create
+    @email = Email.new(object: Faker::Food.dish,
+                      body: Faker::Food.description,
+                      read: false)
+    @email.save
+
+    respond_to do |format|
+      if @email.save
+        format.html { redirect_to emails_path, notice: 'Tu as reçu un nouvel email!' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @email = Email.find(params[:id])
+    @email.destroy
+    redirect_to emails_path, notice: 'Email supprimé !'
+  end
+
+  def toggle
+    @email = Email.find(params[:id])
+    @email.update(read: params[:read])
+
+    render json: { message: 'Success' }
+  end
+end
